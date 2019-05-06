c858861847ed60411d948b7276cd0fe18eac8a42
FilipZg <filip.zgorelec@fer.hr>
2019-05-07 01:09:37 +0200

    dodano mjerenje vremena u test

--- src/test/java/GenerationalGATest.java
+++ src/test/java/GenerationalGATest.java
@@ -10,11 +10,12 @@ import java.util.Random;
 
 public class GenerationalGATest {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         GenerationalGeneticAlgorithm<GenotypeTestDouble> ga = new GenerationalGeneticAlgorithm<>(
                 new GenotypeTestDoubleFactory(),
                 (GenotypeTestDouble genome) -> -(genome.getSolution()[0] * genome.getSolution()[0]
                         + genome.getSolution()[1]* genome.getSolution()[1]+Math.abs(3*genome.getSolution()[2])),
-                new GeneticAlgorithmParameters(30, 1000, 1000),0);
+                new GeneticAlgorithmParameters(100, 100000, 1000),0);
 
         BLXAlphaCross cross = new BLXAlphaCross(2, new Random());
         GaussianMutationDouble mutator = new GaussianMutationDouble(new Random(), 0.3, 1);
@@ -32,11 +33,15 @@ public class GenerationalGATest {
                         return 1;
                     }
                 },
-                genome -> new GenotypeTestDouble(mutator.mutate(genome.getSolution()), 0));;
+                genome -> new GenotypeTestDouble(mutator.mutate(genome.getSolution()), 0));
+        long endTime = System.currentTimeMillis();
+        long timeElapsed = endTime - startTime;
+
         System.out.println(best.getSolution()[0]+"  "+best.getSolution()[1]+"  "+best.getSolution()[2]);
         System.out.println(best.getFitness());
         System.out.println( -(best.getSolution()[0] * best.getSolution()[0]
                 + best.getSolution()[1]* best.getSolution()[1]+Math.abs(3*best.getSolution()[2])));
+        System.out.println("Execution time in milliseconds: " + timeElapsed);
     }
 
 
